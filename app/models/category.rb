6fc6feca1aa30d46522cd59fda651acaf38d428f
Aleksandra <kucharczyk.aleksandra@gmail.com>
2018-11-17 21:06:01 +0100

    Refactor

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,31 @@
 class Category < ApplicationRecord
+  attr_reader :sum_of_operations_values,
+              :sum_of_operations_values_from_last_month,
+              :sum_of_operations_values_from_this_month,
+              :sum_of_operations_count
+
   has_many :operations
   belongs_to :setting, optional: true
 
   validates :name, presence: true, uniqueness: true
+
+  def sum_of_operations_count
+    operations.count
+  end
+
+  def sum_of_operations_values
+    operations.sum(:value_cents) / 100
+  end
+
+  def sum_of_operations_values_from_last_month
+    operations
+      .paid_in_range(Date.current.prev_month.month, Date.current.prev_month.year)
+      .sum(:value_cents) / 100
+  end
+
+  def sum_of_operations_values_from_this_month
+    operations
+      .paid_in_range(Date.current.month, Date.current.year)
+      .sum(:value_cents) / 100
+  end
 end
