753b76f8f5a90f1d23bbfca0bf0fe537d0c4646d
Aleksandra Kucharczyk <kucharczyk.aleksandra@gmail.com>
2018-12-08 11:19:33 +0100

    Fix homepage (#9)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @budget = Budget.order(created_at: :desc).first
-    @entries = @budget.budget_entries.includes(:setting)
+    @entries = @budget&.budget_entries&.includes(:setting)
     @q = Operation.ransack(params[:q])
     @operations = @q.result.includes(:category).newest
   end
