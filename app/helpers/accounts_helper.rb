5eda2a99b341cf63d97097d47354bb9e0f19ee18
Aleksandra Kucharczyk-Buras <kucharczyk.aleksandra@gmail.com>
2018-12-20 21:08:48 +0100

    Refactor

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,5 +1,10 @@
 module AccountsHelper
   def set_chart_data(operations)
-    [{ name: 'A1', stack: 'stack1', data: [['t1', 1],['t2',2],['t3',3],['t4', 4]] }, { name: 'A2', stack: 'stack1', data: [['t1', 1],['t2',2],['t3',3],['t4', 4]] }, { name: 'B1', stack: 'stack2', data: [['t1', 1],['t2',2],['t3',3],['t4', 4]] }]
+
+    [
+      { name: 'B1', stack: 'stack2', data: [['t1', 1],['t2',2],['t3',3],['t4', 4]] },
+      { name: 'A1', stack: 'stack1', data: [['t1', 1],['t2',2],['t3',3],['t4', 4]] },
+      { name: 'A2', stack: 'stack1', data: [['t1', 1],['t2',2],['t3',3],['t4', 4]] }
+    ]
   end
 end
