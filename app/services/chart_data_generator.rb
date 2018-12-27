7ae55bfc470ba9820295047d535a34296bca95db
Aleksandra Kucharczyk-Buras <kucharczyk.aleksandra@gmail.com>
2018-12-27 14:30:29 +0100

    Add proper values for data chart

--- app/services/chart_data_generator.rb
+++ app/services/chart_data_generator.rb
@@ -15,7 +15,7 @@ class ChartDataGenerator
     [
       { name: 'Zaplanowane', stack: 'stack2', data: planned_spendings },
       { name: 'Zaplacone', stack: 'stack1', data: paid },
-      { name: 'Do zaplacenia', stack: 'stack1', data: planned_spendings }
+      { name: 'Do zaplacenia', stack: 'stack1', data: left_to_pay_from_templates }
     ]
   end
 
@@ -32,8 +32,17 @@ class ChartDataGenerator
     end
   end
 
+  def left_to_pay_from_templates
+    entries.map do |entry|
+      not_paid_template_operations_sum = sum_of_template_operations(entry)
+      name = entry.setting.name
+
+      [name, not_paid_template_operations_sum]
+    end
+  end
+
   def entries
-    @entries ||= budget.budget_entries
+    @entries ||= budget.budget_entries.where.not('value_cents > 0')
   end
 
   def sum_of_operations(entry)
@@ -42,6 +51,12 @@ class ChartDataGenerator
                     .operations
                     .paid_in_range(Date.current.month, Date.current.year)
                     .sum(:value_cents)
+    proper_value = (value_cents.abs / 100.00)
+    proper_value
+  end
+
+  def sum_of_template_operations(entry)
+    value_cents = entry.setting.template_operations.not_paid_this_month.sum(:value_cents)
     proper_value = (value_cents.abs / 100.00)
     proper_value
   end
